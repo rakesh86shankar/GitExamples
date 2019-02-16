300361c52f569841e0177c0e56cae2af5bfdb33f
rakesh shankar <rakesh86shankar@gmail.com>
2019-02-16 12:22:06 +0530

    Made changes for master

---
+++ app/src/main/java/com/gradledemo/gittestapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.gradledemo.gittestapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.v("Test is",">>>Master");
+    }
+}

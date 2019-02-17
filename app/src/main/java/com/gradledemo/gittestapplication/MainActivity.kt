d68bdda2db20497642740124a421c95e36561714
rakesh shankar <rakesh86shankar@gmail.com>
2019-02-17 22:51:06 +0530

    variables changes in branch1

--- app/src/main/java/com/gradledemo/gittestapplication/MainActivity.kt
+++ app/src/main/java/com/gradledemo/gittestapplication/MainActivity.kt
@@ -15,8 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     public fun addBranch4()
     {
-        val i = 10;
-        val testCondition = false
+        val i = 15;
+        val testCondition = true;
         Log.v("New Branch Addedt", "Branch4");
         Log.v("New Branch Addedt from Branch2", "Branch4");
     }
